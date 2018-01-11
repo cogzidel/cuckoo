1fa5c2373e1cacaa07fcf6bc71744de3430a0fe1
aservices <arunkumar.g@cogzidel.com>
2018-01-11 18:53:06 +0530

    confirm password hide -Nithya

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,15 +8,15 @@ class UserRegisterForm(forms.Form):
 	username = forms.CharField()
 	email = forms.EmailField()
 	password = forms.CharField(widget=forms.PasswordInput)
-	password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+	# password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
 
-	def clean_password2(self):
-		password = self.cleaned_data.get('password')		
-		password2 = self.cleaned_data.get('password2')
+	# def clean_password2(self):
+	# 	password = self.cleaned_data.get('password')		
+	# 	password2 = self.cleaned_data.get('password2')
 
-		if password != password2:
-			raise forms.ValidationError("Password must match")
-		return password2
+	# 	if password != password2:
+	# 		raise forms.ValidationError("Password must match")
+	# 	return password2
 
 	def clean_username(self):
 		username = self.cleaned_data.get('username')
