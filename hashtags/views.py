3c7aeebf3ac4c2d2a7ec7b3435e0cf1d823199d4
aservices <arunkumar.g@cogzidel.com>
2018-02-01 19:45:20 +0530

    Tag page -- KK

--- hashtags/views.py
+++ hashtags/views.py
@@ -7,8 +7,8 @@ from .models import HashTag
 
 # Create your views here.
 class HashTagView(View):
-	
+
 	def get(self, request, hashtag, *args, **kwargs):
 		obj, create = HashTag.objects.get_or_create(tag=hashtag)
-		return render(request, 'hashtags/tag_view.html', {'obj': obj})
-
+		hashtags = HashTag.objects.all().order_by('-id')[:10]
+		return render(request, 'hashtags/tag_view.html', {'obj': obj, "hashtags":hashtags})
